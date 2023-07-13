2b47ffcf6f501eecf439e6a4cd74c872d21cb820
Atro <103967835+atrogpx@users.noreply.github.com>
2023-07-13 19:51:12 +0200

    keeps score. game works

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-// program to get a random item from an array
+// Program to return randomly r, p, s
 
 function random(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length);
@@ -7,32 +7,55 @@ function random(arr) {
 }
 
 function getComputerChoice() {
-    let items = ["Rock", "Paper", "Scissors"]
+    let items = ["rock", "paper", "scissors"]
     let item = random(items)
     return item
 }
+
+// Capitalize the first letter for declaring the winner
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+// starter score
+
 score = 0
 
-function game() {
-    let playerSelection = prompt("Choose: rock, paper, or scissors?").toLowerCase()
-    let computerSelection = getComputerChoice().toLowerCase()
-    return function(playerSelection, computerSelection) {
-        if (playerSelection === computerSelection) {return alert(`The computer chose ${computerSelection}. It's a tie!`)}
+// playround function takes playerSelection and computerSelection and returns a string
+// playerSelection gets case insensitive
 
-        else if ((playerSelection === "rock" && computerSelection ==="paper") | (playerSelection === "scissors" && computerSelection === "rock") 
-        | (playerSelection === "paper" && computerSelection === "scissors")) {return alert(`Defeat! ${computerSelection.capitalizeFirstLetter()} beats ${playerSelection}.`)}
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) 
+        {alert(`The computer chose ${computerSelection}. It's a tie!`),
+        alert(score)}
+
+    else if ((playerSelection === "rock" && computerSelection ==="paper") 
+        || (playerSelection === "scissors" && computerSelection === "rock") 
+        || (playerSelection === "paper" && computerSelection === "scissors")) 
+        {alert(`Defeat! ${computerSelection} beats ${playerSelection}.`),
+        alert(--score)}
     
-        else if ((computerSelection === "rock" && playerSelection ==="paper") | (computerSelection === "scissors" && playerSelection === "rock") 
-        | (computerSelection === "paper" && playerSelection === "scissors")) {return alert(`Victory! ${playerSelection.capitalizeFirstLetter()} beats ${computerSelection}.`)}
-    };
-}
+    else if ((computerSelection === "rock" && playerSelection ==="paper") 
+        || (computerSelection === "scissors" && playerSelection === "rock") 
+        || (computerSelection === "paper" && playerSelection === "scissors")) 
+        {alert(`Victory! ${playerSelection} beats ${computerSelection}.`),
+        alert(++score)}
+  }
+
+// Loop for 5 rounds
 
 for (let step = 0; step < 5; step++) {
-    let play = game()
-    play()
+    const playerSelection = prompt("Choose: rock, paper, or scissors?").toLowerCase();
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection)
   }
+finalScore(score)
+
+// Declare winner or loser
+
+function finalScore(score) {
+    if (score > 0) {alert(`You win! Final score is: ${score}`)}
+    else if (score === 0) {alert(`It's a tie!  Final score is: ${score}`)}
+    else if (score < 0) {alert(`You lost! Final score is: ${score}`)}
+}
